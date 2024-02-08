734355fee65b2abd6af52cc696689622e1cb944e
Nguyen Huu Phuc Toan <toantoan123toantoan@gmail.com>
2024-02-08 13:01:25 -0500

    h

---
+++ contributing.md
@@ -0,0 +1,69 @@
+# Contributing to Relivator
+
+[![Join the Bleverse Discord](https://discordapp.com/api/guilds/1075533942096150598/widget.png?style=banner2)][bleverse-discord]
+
+Thank you for your interest in contributing to Bleverse Relivator. Your involvement is fundamental to the success of this open-source project!
+
+## Getting Started
+
+- **Fork and Setup**: Begin by forking the repository. For setup instructions, see [README.md](/README.MD).
+- **Code of Conduct**: Please read our [Code of Conduct](./CODE_OF_CONDUCT.md). We expect all participants to adhere to it.
+
+## How to Contribute
+
+### Legal Requirements
+
+- **Authorship**: Ensure you have authored 100% of the content you contribute.
+- **Rights**: Confirm that you have the necessary rights to your contributions.
+- **License**: Contributions are provided under the project's license.
+
+### Pull Requests
+
+- **UI Changes**: Use the [UI Pull Request Template](.github/issue_template/2.feature_request.yml). Attach screenshots for easier review.
+- **Other Contributions**: For non-UI changes, use the [Other Template](.github/issue_template/2.feature_request.yml). Be thorough in your description.
+
+### Reporting Bugs
+
+#### Before Submitting
+
+- **Research**: Ensure your bug hasn't already been reported.
+- **Information**: Gather all relevant information to describe the issue comprehensively.
+
+#### Submitting a Bug Report
+
+- **Use GitHub Issues**: Report bugs via [GitHub Issues](/issues/new).
+- **Describe**: Clearly explain the expected and actual behaviors, and provide steps to reproduce.
+- **Privacy**: Do not report security vulnerabilities here. Email [blefnk@gmail.com](mailto:blefnk@gmail.com) for sensitive issues.
+
+### Suggesting Enhancements
+
+#### Before Suggesting
+
+- **Latest Version**: Verify you're using the most recent version.
+- **Search**: Check if the enhancement has been previously suggested in [GitHub Issues](/issues) or our [Suggestions Channel on Discord](https://discord.com/channels/796594544980000808/1143947327892103188).
+- **Relevance**: Ensure your suggestion aligns with the project's goals.
+
+#### Submitting an Enhancement
+
+- **Title**: Use a clear, descriptive title.
+- **Details**: Provide a step-by-step description of the enhancement.
+- **Rationale**: Explain why this enhancement would be beneficial.
+
+### Your First Code Contribution
+
+1. **Select an Issue**: Pick an issue from [GitHub issues](/issues) and get it assigned to you.
+2. **Fork and Branch**: Fork the repo and create a branch. Include the issue number in the branch name.
+3. **Commit**: Follow a clear commit style, referencing the issue in the PR or commit message.
+4. **Pull Request**: Submit a PR from your forked repository.
+
+## Helpful Resources
+
+### Commit Examples
+
+| ✅ Good Example                                       | ❌ Poor Example  |
+| ----------------------------------------------------- | ---------------- |
+| 🌐 add(i18n): add internationalization support to rsc | YOOOOOO          |
+| 🛒 fix(uiux): move the cart to right for user habits  | fixing the thing |
+| 📚 upd(docs): improve some typos in the readme file   | Trust me bro     |
+
+[bleverse-discord]: https://discord.gg/Pb8uKbwpsJ
---
+++ src/app/[locale]/showoff_video/video_showoff.tsx
@@ -0,0 +1,167 @@
+/* eslint-disable style/max-statements-per-line */
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+
+import HeroImage from "./hero-image2.png";
+import Modal from "./Modal";
+
+export function HeroHome() {
+  const [videoModalOpen, setVideoModalOpen] = useState(false);
+
+  return (
+    <section className="relative">
+      {/* Illustration behind hero content */}
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
+
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        {/* Hero content */}
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          {/* Section header */}
+          <div className="text-center pb-12 md:pb-16">
+            <div className="max-w-3xl mx-auto">
+              <p
+                className="text-xl text-gray-600 mb-8"
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                Rapid Innovation serves as a valuable partner in unlocking the
+                potential of AI-driven Data visualizer, including AI object
+                recognition, AI video text recognition, and AI image feature
+                analysis. Target your audience with precision, from age and
+                gender to location and interests. Our extensive services covers
+                Human Pose Estimation, Defect & Predictive Maintenance, Object
+                Detection, Video Analysis, Emotion Detection and Custom Image
+                Recognition. Elevate your business with cutting-edge AI
+                solutions.
+              </p>
+            </div>
+          </div>
+
+          {/* Hero image */}
+          <div>
+            <div
+              className="relative flex justify-center mb-8"
+              data-aos="zoom-y-out"
+              data-aos-delay="450"
+            >
+              <div className="flex flex-col justify-center">
+                <Image
+                  className="mx-auto"
+                  src={HeroImage}
+                  width="768"
+                  height="432"
+                  alt="Hero"
+                />
+                {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+                <svg
+                  className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto"
+                  width="768"
+                  height="432"
+                  viewBox="0 0 768 432"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                  <g fill="none" fillRule="evenodd">
+                    <circle
+                      fillOpacity=".04"
+                      fill="url(#hero-ill-a)"
+                      cx="384"
+                      cy="216"
+                      r="128"
+                    />
+                    <circle
+                      fillOpacity=".16"
+                      fill="url(#hero-ill-b)"
+                      cx="384"
+                      cy="216"
+                      r="96"
+                    />
+                    <g fillRule="nonzero">
+                      <use fill="#000" xlinkHref="#hero-ill-d" />
+                      <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
+                    </g>
+                  </g>
+                </svg>
+              </div>
+              <button
+                type="button"
+                className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg"
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  try {
+                    setVideoModalOpen(true);
+                    console.log("1223123123123123");
+                  } catch (error) {
+                    console.error("Error opening video modal:", error);
+                  }
+                }}
+                aria-controls="modal"
+              >
+                <svg
+                  className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 flex-shrink-0"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
+                  <path d="M10 17l6-5-6-5z" />
+                </svg>
+                <span className="ml-3 text-gray-600">
+                  Watch the full video (1 min)
+                </span>
+              </button>
+            </div>
+
+            {/* Modal */}
+            <Modal
+              id="modal"
+              ariaLabel="modal-headline"
+              show={videoModalOpen}
+              handleClose={() => setVideoModalOpen(false)}
+            >
+              <div className="pb-9/16">
+                <iframe
+                  className="absolute inset-20 w-3/4 h-3/4 mx-auto my-auto"
+                  src="https://player.vimeo.com/video/909827526?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                  title="Video"
+                  allowFullScreen
+                />
+              </div>
+            </Modal>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
